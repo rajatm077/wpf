0491ceccb0a647b6765eac34a342276abb1655ea
MalhotR1 <rajatmalhotra26@gmail.com>
2017-09-09 00:51:22 +0530

    Changing colour of treeviewitem using code behind

--- Directory/MainWindow.xaml.cs
+++ Directory/MainWindow.xaml.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,6 +20,18 @@ namespace Directory {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            level1.Foreground = Brushes.Red;
+            level2.Foreground = Brushes.Green;
+        }
+
+        private void btnLevel1_Click(object sender, RoutedEventArgs e) {
+            level1.Foreground = Brushes.White;
+        }
+
+        private void btnLevel2_Click(object sender, RoutedEventArgs e) {
+            level2.Foreground = Brushes.Yellow;
+
+
         }
     }
 }
